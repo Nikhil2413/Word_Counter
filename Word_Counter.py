49e7c099028df70f9fb47e24656711518acabe9a
Nikhil2413 <115886705+Nikhil2413@users.noreply.github.com>
2025-02-15 14:27:53 +0530

    Update Word_Counter.py

--- Word_Counter.py
+++ Word_Counter.py
@@ -1,6 +1,5 @@
 def word_counter():
     print("Enter your text (Press Enter on an empty line to finish):\n")
-    
     lines = []
     while True:
         line = input()
@@ -12,5 +11,5 @@ def word_counter():
     words = text.split()
     word_count = len(words)
     print(f"\nTotal word count: {word_count}")
-
+    
 word_counter()
