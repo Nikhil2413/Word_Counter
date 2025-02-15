5ae5548cc589047b4011cb161c3c9ce23473bf74
Nikhil2413 <115886705+Nikhil2413@users.noreply.github.com>
2025-02-15 14:27:15 +0530

    Add files via upload

---
+++ Word_Counter.py
@@ -0,0 +1,16 @@
+def word_counter():
+    print("Enter your text (Press Enter on an empty line to finish):\n")
+    
+    lines = []
+    while True:
+        line = input()
+        if line == "":
+            break
+        lines.append(line)
+
+    text = " ".join(lines)
+    words = text.split()
+    word_count = len(words)
+    print(f"\nTotal word count: {word_count}")
+
+word_counter()
